6ea347b4fb614e1aa2fa193e25689385b55ddc61
mounaad <admantmouna@gmail.com>
2026-02-12 12:33:04 +0100

    resoudre issues4

--- src/main/java/servlet/AdminDashboardServlet.java
+++ src/main/java/servlet/AdminDashboardServlet.java
@@ -5,7 +5,10 @@ import java.sql.Connection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import dao.ClientDao;
 import dao.ClientDaoImp;
 import dao.CoiffeurDao;
