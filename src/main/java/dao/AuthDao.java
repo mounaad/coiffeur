432cd41093a00f1824835505c626b7cd77efa84d
mounaad <admantmouna@gmail.com>
2026-02-12 12:42:46 +0100

    resoudre issues5

--- src/main/java/dao/AuthDao.java
+++ src/main/java/dao/AuthDao.java
@@ -4,30 +4,30 @@ import java.sql.*;
 import model.User;
 
 public class AuthDao {
-	private Connection conn;
+    private Connection conn;
 
-	 public AuthDao(Factory factory) {
-	        try {
-				this.conn = Factory.getConnection();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-	    }
+    public AuthDao(Factory factory) {
+        try {
+            this.conn = Factory.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to get database connection", e);
+        }
+    }
+
     public User login(String usernameOrEmail, String password) throws SQLException {
-    	 User user = null;
-        
+        User user = null;
+
         String sqlAdmin = "SELECT * FROM admin WHERE username=? AND mot_de_passe=?";
         try (PreparedStatement pst = conn.prepareStatement(sqlAdmin)) {
             pst.setString(1, usernameOrEmail);
             pst.setString(2, password);
             try (ResultSet rs = pst.executeQuery()) {
-                if (rs.next()) { 
+                if (rs.next()) {
                     return new User(rs.getInt("id_admin"), rs.getString("username"), "admin");
                 }
             }
         }
 
-        
         String sqlCoiffeur = "SELECT * FROM coiffeur WHERE email=? AND mot_de_passe=? AND statut='valide'";
         try (PreparedStatement pst = conn.prepareStatement(sqlCoiffeur)) {
             pst.setString(1, usernameOrEmail);
@@ -39,7 +39,6 @@ public class AuthDao {
             }
         }
 
-        
         String sqlClient = "SELECT * FROM client WHERE email=? AND mot_de_passe=?";
         try (PreparedStatement pst = conn.prepareStatement(sqlClient)) {
             pst.setString(1, usernameOrEmail);
